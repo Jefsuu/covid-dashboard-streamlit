e5f43fed8286983fc3eba818e8d4ea5af0a80d8b
unknown <jdjbubu28@gmail.com>
2021-04-10 16:05:01 -0300

    Update dashboard.py
    Adição do gráfico de barras e atualização da estrutura do dashboard

--- dashboard.py
+++ dashboard.py
@@ -13,8 +13,18 @@ df['date'] = pd.to_datetime(df['date']).dt.date
 df.rename(columns={'location':'country'}, inplace=True)
 vaccines = pd.read_csv('https://raw.githubusercontent.com/Jefsuu/covid-dashboard-streamlit/main/vaccine_country.csv', usecols=['country', 'vaccines'])
 
+#Configurações da pagina
+st.set_page_config(
+    layout="centered",
+    initial_sidebar_state="expanded",
+    page_title="Dashboard Covid-19",
+    )
+
 #paginas
-paginas = st.selectbox('Selecione o tema desejado',['Vacinação', 'Vacinas'])
+st.sidebar.title('Navegação')
+paginas = st.sidebar.radio('Paginas', ['Vacinação', 'Vacinas'])
+
+
 
 #lista de paises
 paises = df.country.unique().tolist()
@@ -38,12 +48,12 @@ if paginas == 'Vacinação':
     
 
     # Multiselect com os paises
-    label_to_filter = st.sidebar.multiselect(
+    label_to_filter = st.multiselect(
         label="Escolha o Pais desejado para visualizar os dados",
         options=paises, default='Brazil'
     )
 
-    if st.sidebar.checkbox('Mostrar tabela'):
+    if st.checkbox('Mostrar tabela'):
         st.subheader('Tabela de dados')
         group = df[df['country'].isin(label_to_filter)]
         group = group.groupby(['country'])['daily_vaccinations'].agg(['mean', 'min', 'max', 'sum'])
@@ -68,16 +78,36 @@ if paginas == 'Vacinação':
 
     st.plotly_chart(fig, use_container_width=True)
 
+    #grafico de barras
+    bar_plot = df.groupby(by=['country'])['daily_vaccinations'].sum()
+    bar_plot.sort_values(ascending=False, inplace=True)
+    fig = px.bar(bar_plot, range_x=[-0.6,25.5], labels={'country':'Pais', 'value':'Nº de vacinações'})
+    fig.update_layout(barmode='group', xaxis_tickangle=-45, xaxis={})
+    fig.update_layout(
+        title={
+            'text': "<b>Numero total de vacinações por Pais</b>",
+            'y':0.95,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'},
+            showlegend=False)
+
+    st.plotly_chart(fig, use_container_width=True)
+
 
 if paginas == 'Vacinas':
 
+    
     st.title('Vacinas ultilizadas em cada Pais')
+    st.markdown("""
+    Na tabela abaixo, pode ser visualizada as vacinas que estão sendo utilizadas por cada pais.
+    E na nuvem de palavras é possivel ver quais são as vacinas que mais estão sendo utilizadas no mundo.""")
     # Multiselect com os paises
-    label_to_filter = st.sidebar.multiselect(
+    label_to_filter = st.multiselect(
         label="Escolha o Pais desejado para visualizar os dados",
         options=paises, default='Brazil'
     )
-    if st.sidebar.checkbox('Mostrar tabela', value=True):
+    if st.checkbox('Mostrar tabela', value=True):
         st.dataframe(vaccines[vaccines['country'].isin(label_to_filter)])
 
     st.markdown("""
@@ -89,7 +119,20 @@ if paginas == 'Vacinas':
     st.markdown("<h4 style='text-align: center; color: black;'>Vacinas mais utilizadas</h4>", unsafe_allow_html=True)
     st.image(image)
 
+#progresso
+st.sidebar.title('Desenvolvimento')
+st.sidebar.info("""
+    Dados que serão adicionados:
+    * Testagem de Covid-19
+    * Covid no Brasil""")
 
+#informações de contato
+st.sidebar.title('Sobre')
+st.sidebar.info("""
+    Dashboard criado por Jeferson Ribeiro
+    
+    [Github](https://github.com/Jefsuu)
+    [Kaggle](https://www.kaggle.com/jefsuu)""")
 
     
 
